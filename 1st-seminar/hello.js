7ccec817e58f558820e177aa60f19d2ff791c607
YeongEunLee <zduddmszz@gmail.com>
2020-10-10 20:22:10 +0900

    first commit

---
+++ 1st-seminar/hello.js
@@ -0,0 +1,25 @@
+var variableVar = "123";
+var variableVar = "321";
+
+console.log('variableVar: ${variableVar}');
+
+let vl = "123";
+let vl = "321";
+
+console.log('vl : ${vl}');
+
+const vc = "123";
+const vc = "321";
+
+console.log('vc : ${vc}');
+
+if(true){
+    var x = 'var';
+}
+console.log('var : ${x}');
+
+if(true){
+    let y = 'let';
+}
+console.log('let : ${y}');
+
