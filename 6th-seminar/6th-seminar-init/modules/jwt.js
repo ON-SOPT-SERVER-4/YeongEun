d34944ae5ddfdae5ea98b92336310560924025e3
YeongEunLee <zduddmszz@gmail.com>
2020-12-15 14:42:49 +0900

    6th seminar

--- 6th-seminar/6th-seminar-init/modules/jwt.js
+++ 6th-seminar/6th-seminar-init/modules/jwt.js
@@ -3,7 +3,10 @@ const {
     secretKey,
     options,
     refreshOptions
-} = require('../config/secretKey');
+} = require('../config/secretKey')
+const {
+    userService
+} = require('../service');
 const TOKEN_EXPIRED = -3;
 const TOKEN_INVALID = -2;
 
@@ -14,9 +17,10 @@ module.exports = {
             name: user.userName
         };
         const result = {
-            accessToken: jwt.sign(payload, secretKey, options),
+            token: jwt.sign(payload, secretKey, options),
             refreshToken: jwt.sign(payload, secretKey, refreshOptions),
         };
+        await userService.updateRefreshToken(user.id, result.refreshToken);
         return result;
     },
     verify: async (token) => {
@@ -37,5 +41,38 @@ module.exports = {
             }
         }
         return decoded;
+    },
+    refresh: async (refreshToken) => {
+        try {
+            const result = await jwt.verify(refreshToken, secretKey);
+            if (result.id === undefined) {
+                return TOKEN_INVALID;
+            }
+            const user = await userService.getUserId(result.id);
+            if (refreshToken !== user.refreshToken) {
+                console.log('invalid refresh token');
+                return TOKEN_INVALID;
+            }
+            const payload = {
+                id: user.id,
+                name: user.userName
+            };
+            const dto = {
+                token: jwt.sign(payload, secretKey, options)
+            };
+            return dto;
+        } catch (err) {
+            if (err.message === 'jwt expired') {
+                console.log('expired token');
+                return TOKEN_EXPIRED;
+            } else if (err.message === 'invalid token') {
+                console.log('invalid token');
+                console.log(TOKEN_INVALID);
+                return TOKEN_INVALID;
+            } else {
+                console.log("invalid token");
+                return TOKEN_INVALID;
+            }
+        }
     }
 }
