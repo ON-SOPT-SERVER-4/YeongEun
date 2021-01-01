f69dc17caf0fc78c01ad221f4d159e21b0f22309
YeongEunLee <zduddmszz@gmail.com>
2021-01-01 19:56:52 +0900

    Fix UI bug

--- 1st-seminar/array.js
+++ 1st-seminar/array.js
@@ -6,17 +6,24 @@ var arr1 = [];
 console.log(arr1);
 console.log(typeof arr1);
 
-var arr2 = new Array(1,2,3,4,5);
+var arr2 = new Array(1, 2, 3, 4, 5);
 console.log(arr2);
 console.log(typeof arr2);
 
-var arr3 = ['이영은', 1, 2, 3, null, {name: 'ye', age : 24}];
+var arr3 = ['이영은', 1, 2, 3, null, {
+    name: 'ye',
+    age: 24
+}];
+var arr4 = ['김정재', 1, 2, 3, null, {
+    name: 'ye',
+    age: 26
+}];
 console.log(arr3);
 console.log(typeof arr3);
 
 /**
  * 2. array prototype 메서드
-**/
+ **/
 
 console.log('**** Array 기본 함수들을 알아보자 ****');
 var arr = [1, 2, 3, 4];
@@ -59,25 +66,29 @@ console.log(location.reverse().join('-> '));
 
 // 2-9 sort
 var countries = ['Österreich', 'Andorra', 'Vietnam'];
-console.log(countries.sort( (a, b) => a > b ? 1 : -1) ); // Andorra, Vietnam, Österreich (제대로 정렬이 되지 않았습니다.)
-console.log(countries.sort( function(a, b) { return a.localeCompare(b); })); // Andorra,Österreich,Vietnam (제대로 정렬되었네요!) 유니코드 기준으로 문자 정렬
+console.log(countries.sort((a, b) => a > b ? 1 : -1)); // Andorra, Vietnam, Österreich (제대로 정렬이 되지 않았습니다.)
+console.log(countries.sort(function (a, b) {
+    return a.localeCompare(b);
+})); // Andorra,Österreich,Vietnam (제대로 정렬되었네요!) 유니코드 기준으로 문자 정렬
 console.log('오름차순 정렬:', concatArr.sort((a, b) => a - b));
-console.log('내림차순 정렬:', concatArr.sort(function(a, b) { return  b - a; }));
+console.log('내림차순 정렬:', concatArr.sort(function (a, b) {
+    return b - a;
+}));
 
 // 2-10 filter 필터는 배열 요소 전체를 대상으로 조건을 걸어서 그 조건을 충족하는 결과를 새로운 배열을 반환해줌.
 var number = [100, 234, -125, 1, 23, -637, -123, 99, 2, 3, 4, 5];
-var minusNumber = number.filter( item => item < 0);
-console.log('minusNumber: ', minusNumber); 
+var minusNumber = number.filter(item => item < 0);
+console.log('minusNumber: ', minusNumber);
 
 // 2-11 map  map은 배열 요소 전체를 대상으로 함수를 호출하고, 그 결과를 새로운 배열을 반환할때 주로 사용
 var countries = ['Österreich', 'Andorra', 'Vietnam', 'Korea', 'China'];
-var countriesLengths = countries.map( item => item.length );
-console.log('countriesLengths: ',countriesLengths);
+var countriesLengths = countries.map(item => item.length);
+console.log('countriesLengths: ', countriesLengths);
 
 
 // 2-12 reduce map은 배열을 반환할때 사용했지만 reduce는 값 하나를 반환할때 주로 사용 대표적인 예시로 1 ~ n 까지 더하기
 var number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-var sum = number.reduce( (previousValue, currentValue) => {
+var sum = number.reduce((previousValue, currentValue) => {
     console.log(`previousValue: ${previousValue}, currentValue: ${currentValue}`);
     return previousValue + currentValue;
 });
@@ -92,16 +103,16 @@ var serverPart = ["김현기", "석영현", "강준우", "송정우", "신지혜
 let serverIndexStr = '서버파트 여러분 번호 한번 세겠습니다. "';
 let serverPartMemberNameStr = '서버파트 여러분 이름 한번씩만 불러주세요~ "'
 
-for(let item in serverPart){
+for (let item in serverPart) {
     serverIndexStr += item + '! ';
 }
 console.log(serverIndexStr);
 
-for(let item of serverPart) {
+for (let item of serverPart) {
     serverPartMemberNameStr += item + '! ';
 }
 console.log(serverPartMemberNameStr);
 
-serverPart.forEach( item => {
+serverPart.forEach(item => {
     console.log(item);
 })
